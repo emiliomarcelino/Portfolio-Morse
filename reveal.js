b6a3dc906584a87b13b080f54430e82a5319f30f
emiliomarcelino <emiliomarcelino400@gmail.com>
2025-04-07 04:23:49 -0700

    jjjjjj

---
+++ reveal.js
@@ -0,0 +1,85 @@
+window.revelar = ScrollReveal({reset:true})
+
+//Topo do site
+revelar.reveal('.efeito-txt-topo',{
+    duration: 2000, 
+    distance: '90px',
+})
+
+revelar.reveal('.efeito-img-topo',{
+    duration: 2000, 
+    distance: '90px',
+    delay: 400
+})
+
+//titulos
+revelar.reveal('.titulo',{
+    duration: 2000,
+    distance: '150px'
+})
+
+revelar.reveal('.titulo2',{
+    duration: 2000,
+    distance: '150px'
+})
+
+//SKILLS
+revelar.reveal('.skills-box1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.skills-box2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.skills-box3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.skills-box4',{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
+
+//About
+revelar.reveal('.efeito-img-about', {
+    duration: 3000,
+    distance: '90px',
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-txt-about', {
+    duration: 4000,
+    distance: '90px',
+    delay: 400,
+    origin: 'right'
+})
+
+//Our projects
+revelar.reveal('.img-port1', {
+    duration: 2000,
+    distance: "90px",
+    delay: 400,
+    origin: 'top'
+})
+
+revelar.reveal('.img-port2', {
+    duration: 2000,
+    distance: "90px",
+    delay: 600,
+    origin: 'bottom'
+})
+
+revelar.reveal('.img-port3', {
+    duration: 2000,
+    distance: "90px",
+    delay: 800,
+    origin: 'top'
+})
